c9382923b714a0b7e5588e6993a34d715b309c7b
Ursula Mayer <ursulamayer123@gmail.com>
2025-12-06 15:05:23 -0300

    Ejercicio clase 10

---
+++ clase10/10ejercicio5.js
@@ -0,0 +1,12 @@
+const prompt = require ('prompt-sync')();
+let primerNumero = 30;
+let segundoNumero = 40;
+let resultadoSuma = primerNumero + segundoNumero;
+let resultadoResta = primerNumero - segundoNumero;
+let resultadoMultiplicacion = primerNumero * segundoNumero;
+let resultadoDivision = primerNumero / segundoNumero;
+
+console.log("Suma: " + resultadoSuma);
+console.log("Resta: " + resultadoResta);
+console.log("Multiplicación: " + resultadoMultiplicacion);
+console.log("División: " + resultadoDivision);
